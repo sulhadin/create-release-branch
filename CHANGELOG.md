a729664693fb3dec10e9ddbab21a8f721f94bb28
sulhadin <sulhadin@gmail.com>
2025-06-27 23:25:19 +0300

    enhancements

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [1.2.0] - 2025-06-23
+
+### Other Changes\n\n- Refine release branch workflow inputs in README ([#68](https://github.com//pull/68)) ([6c6b213](https://github.com//commit/6c6b213))\n\n
+## [1.1.0] - 2025-06-22
+
+### Other Changes\n\n- #ABC - Commit 1 ([#49](https://github.com//pull/49)) ([bb92243](https://github.com//commit/bb92243))\n\n
+## [1.0.0] - 2025-06-22
+## [1.0.0] - 2025-06-22
+
+### Other Changes
+
+- Add create release workflow. ([#57](https://github.com//pull/57)) ([4fe1cd9](https://github.com//commit/4fe1cd9))
+
--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 new_version=$1
+version_file=$2
 
 if [ -z "$new_version" ]; then
-  echo "Usage: sh update_version.sh <version>"
+  echo "Usage: sh update_version.sh <version> <version_file>"
   exit 1
 fi
 
-jq ".version = \"$new_version\"" version.json > temp.json && mv temp.json version.json
+if [ -z "$version_file" ]; then
+  echo "Usage: sh update_version.sh <version> <version_file>"
+  exit 1
+fi
+
+
+#jq ".version = \"$new_version\"" version.json > temp.json && mv temp.json version.json
+jq ".version = \"$new_version\"" "$version_file" > "${version_file}.tmp" && mv "${version_file}.tmp" "$version_file"
+
 
 echo "version.json: Updated version to $new_version"
